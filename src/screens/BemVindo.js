44592332a22b4d52093d85fd5f46b2d4506f73f5
DESKTOP-GO7PEOM\DELL <fabriciolugao@gmail.com>
2024-04-18 14:16:26 -0300

    Fix StyleSheet import

--- src/screens/BemVindo.js
+++ src/screens/BemVindo.js
@@ -1,26 +1,25 @@
-
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
 function BemVindo() {
-    return (
-        <View style={styles.container}> <Text> Teste tela de 
-            <Text style={styles.text}> Bem Vindo</Text>
-            </Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text>
+        Teste tela de <Text style={styles.text}>Bem Vindo</Text>
+      </Text>
+    </View>
+  );
 }
-export default BemVindo
+export default BemVindo;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    text: {
-        fontSize: 24, 
-        color: 'red', 
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    color: "red",
+  },
 });
-
